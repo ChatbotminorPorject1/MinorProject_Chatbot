88d09f2cac27e63777daffb4c612927801ecf74a
ChatbotminorPorject1 <101899301+ChatbotminorPorject1@users.noreply.github.com>
2022-04-19 18:00:26 +0530

    Add files via upload

---
+++ DataRequests/MakeApiRequests.py
@@ -0,0 +1,51 @@
+import requests
+import json
+class Api:
+    def __init__(self):
+        pass
+
+    def makeApiRequestForCounrty(self, country_name):
+        url = "https://covid-193.p.rapidapi.com/countries"
+        querystring = {"country": country_name}
+        headers = {
+            'x-rapidapi-host': "covid-193.p.rapidapi.com",
+            'x-rapidapi-key': "36626c56f2msh8580d29c47aca59p12a368jsnc74c1cdf1888"
+        }
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        # print(response.text)
+        js = json.loads(response.text)
+        print("******", js)
+        result = js.get('response')[0]
+        print(result.get('cases'))
+        print("*" * 20)
+        return result.get('cases') , result.get('deaths'),result.get('tests')
+
+
+    def makeApiRequestForIndianStates(self):
+        url = "https://covid-193.p.rapidapi.com/statistics"
+        headers = {
+            'x-rapidapi-host': "covid-193.p.rapidapi.com",
+            'x-rapidapi-key': "36626c56f2msh8580d29c47aca59p12a368jsnc74c1cdf1888"
+        }
+        response = requests.request("GET", url, headers=headers)
+        # print(response.text)
+        js = json.loads(response.text)
+        print("******", js)
+        #result = js.get('list')
+        return js
+
+
+    def makeApiWorldwide(self):
+        url = "https://covid-193.p.rapidapi.com/history?country=india&day=2022-04-17"
+        headers = {
+            "x-rapidapi-host": "covid-193.p.rapidapi.com",
+            "x-rapidapi-key": "36626c56f2msh8580d29c47aca59p12a368jsnc74c1cdf1888"
+        }
+        response = requests.request("GET", url, headers=headers)
+        # print(response.text)
+        js = json.loads(response.text)
+        print("******", js)
+        result = js.get('data')
+
+        return result
+
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# MinorProject_Chatbot
-This is a NLP based chatbot, build and maintain on the google powered NLP based framework (Dialogflow). Also use python(flask) and Rapid API to render statistics regarding the COVID_19 cases. Also use MongoDB to store the user bot conversation for future training of the chatbot.
+
+# End-to-End Covid-19 chatbot using DialogFlow,Python/Flask,MongoDb Atlas and deployed on Pivotal Cloud foundary and Telegram
+This chatbot developed using Dialoglow,python,flask,MongoDB and deployed on Telegram. search @bestcovid19_bot on telegram app
+BestCovid-19 Bot can give answers of all of your queries related to covid-19 also it can -
+
+
+We have also mentioned how to test it locally on PostMan.
+
+
+1). Show us statisics based on Country,Indian States,Indian districts,Google Maps,WorldWide
+2). can send Email to user email Address.
+3). Can show Live news
+4). Govt annoucements , videos and Images.
+
+How to Setup the whole system ? 
+1. Setup Pivotal Cloud foundary account and install foundary CLI to your system
+
+
+2. Setup MongoDB Atlas account
+
+
+3. Setup Dialogflow Account
+
+
+4. Download and Install Pycharm or any IDE to open my code.
+
+
+5. use PostMan to test this code locally. localhost:5000/webhook , give header and body as Raw json.
+
